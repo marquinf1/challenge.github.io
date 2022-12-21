a65145b9d941568141bb549d79d851c2b3eb4479
Marcos Ferraz <marcos.ferraz@ufvjm.edu.br>
2022-12-21 13:46:07 -0300

    Refinamento

--- script.js
+++ script.js
@@ -1,35 +1,41 @@
 function criptografar() {
 	// obtém a palavra a ser criptografada
 	var palavra = document.getElementById("palavra").value;
+	var palavra1 = document.getElementById("palavra1").value;
 
 	// cria um dicionário de substituição de letras
 	var dicionario = {
-		a: "z",
-		b: "y",
-		c: "x",
-		d: "w",
-		e: "v",
-		f: "u",
-		g: "t",
-		h: "s",
-		i: "r",
-		j: "q",
-		k: "p",
-		l: "o",
-		m: "n",
-		n: "m",
-		o: "l",
-		p: "k",
-		q: "j",
-		r: "i",
-		s: "h",
-		t: "g",
-		u: "f",
-		v: "e",
-		w: "d",
-		x: "c",
-		y: "b",
-		z: "a"
+		a: "ai",
+		//ai: "a",
+		b: "b",
+		c: "c",
+		d: "d",
+		e: "enter",
+		//enter: "e",
+		f: "f",
+		g: "g",
+		h: "h",
+		i: "imes",
+		//imes: "i",
+		j: "j",
+		k: "k",
+		l: "l",
+		m: "m",
+		n: "n",
+		o: "ober",
+		//ober: "o",
+		p: "p",
+		q: "q",
+		r: "r",
+		s: "s",
+		t: "t",
+		u: "ufat",
+		//ufat: "u",
+		v: "v",
+		w: "w",
+		x: "x",
+		y: "y",
+		z: "z"
 	};
 
 	// criptografa a palavra
@@ -38,6 +44,14 @@ function criptografar() {
 		criptografada += dicionario[palavra[i]];
 	}
 
+	var criptografada1 = "";
+	for (var i = 0; i < palavra1.length; i++) {
+		criptografada1 += dicionario[palavra1[i]];
+	}
+
 	// exibe a palavra criptografada
 	document.getElementById("resultado").innerHTML = "Palavra criptografada: " + criptografada;
+
+	// exibe a palavra descriptografada
+	document.getElementById("resultado").innerHTML = "Palavra criptografada: " + criptografada1;
 }
